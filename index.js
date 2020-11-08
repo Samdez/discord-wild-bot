7870c436cedf8d28e941221962f2b07b8c9b74a1
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-11-08 22:56:39 +0100

    test

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on('ready', () => {
     const seconds = time.getSeconds();
 
 
-    if (hours === 22 && minutes === 55) {
+    if (hours === 22 && minutes === 58) {
       if (!messageHasBeenSent) {
         testChannel.send('Time for the daily meet!');
         messageHasBeenSent = true;
