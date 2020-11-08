6b5005d3901a9937f45cbc7f2088297e668fcec7
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-11-08 22:44:51 +0100

    changed to node index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ client.on('ready', () => {
     const seconds = time.getSeconds();
 
 
-    if (hours === 22 && minutes === 38) {
+    if (hours === 22 && minutes === 44) {
       if (!messageHasBeenSent) {
         testChannel.send('Time for the daily meet!');
         messageHasBeenSent = true;
