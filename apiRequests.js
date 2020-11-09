87de395d63a344ac71a646b8b5e78330da883e56
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-11-09 09:46:18 +0100

    changed number of post requests

--- apiRequests.js
+++ apiRequests.js
@@ -1,6 +1,6 @@
 const { default: Axios } = require("axios");
 
-const programmerHumorRequest = Axios.get('https://www.reddit.com/r/ProgrammerHumor.json?count=5')
+const programmerHumorRequest = Axios.get('https://www.reddit.com/r/ProgrammerHumor.json?count=1')
 .then(res => {
   return res.data.data.children.url_overridden_by_dest;
 });
