0cf4c256264cbb242ff67f71d20d2c4d385aec05
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-11-09 09:51:18 +0100

    changed token

--- apiRequests.js
+++ apiRequests.js
@@ -1,6 +1,6 @@
 const { default: Axios } = require("axios");
 
-const programmerHumorRequest = Axios.get('https://www.reddit.com/r/ProgrammerHumor.json?count=1')
+const programmerHumorRequest = Axios.get('https://www.reddit.com/r/ProgrammerHumor.json?count=1ù')
 .then(res => {
   return res.data.data.children.url_overridden_by_dest;
 });
