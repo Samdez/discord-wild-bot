16b8f009fb868523a2fccf30730f22cd386dcf7e
De Zaldua Samuel <dezalduasamuel@MacBook-Pro-de-De.local>
2020-11-10 14:36:34 +0100

    test

--- index.js
+++ index.js
@@ -29,49 +29,11 @@ client.on('ready', () => {
   cron.schedule('00 14 * * *',  () => {
     testChannel.send('FINALLY', {files: ['./img/itWorks.jpg']});
   })
-  cron.schedule('45 15 * * *', () => {
-    wildChannel.send('Time for the daily meet! See you @ meet.google.com/gcq-hiur-nye');
-  });
+  // cron.schedule('45 15 * * *', () => {
+  //   wildChannel.send('Time for the daily meet! See you @ meet.google.com/gcq-hiur-nye');
+  // });
 
-  // let messageHasBeenSent = false;
   
-  // testChannel.send('FINALLY', {files: ['./img/itWorks.jpg']});
-  // const getTime = () => {
-  //   const time = new Date();
-  //   const hours = time.getHours();
-  //   const minutes = time.getMinutes();
-
-  //   if (hours === 08 && minutes === 55) {
-  //     if (!messageHasBeenSent) {
-  //       wildChannel.send('Good morning Wilders!');
-  //       messageHasBeenSent = true;
-  //       // const dailyPost = request.programmerHumorRequest;
-  //       // testChannel.send(dailyPost);
-  //     }
-  //   }
-  //   if (hours === 20 && minutes === 00) {
-  //     if (!messageHasBeenSent) {
-  //       testChannel.send('timer test');
-  //       testChannel.send('FINALLY', {files: ['./img/itWorks.jpg']});
-  //       // wildChannel.send('Time for the daily meet! See you @ meet.google.com/gcq-hiur-nye');
-  //       messageHasBeenSent = true;
-  //     }
-  //   }
-  //   if ((hours === 09 && minutes === 05) || (hours === 17 && minutes === 00)) {
-  //     messageHasBeenSent = false;
-  //   }
-  // }
-
-  // setInterval(() => {
-  //   getTime();
-  // }, 1000)
 
 })
 
-// client.on('message', msg => {
-//   if (msg.content === 'ping') {
-//     msg.reply('Pong!');
-//   }
-// });
-
-// console.log(process.env.TOKEN)
